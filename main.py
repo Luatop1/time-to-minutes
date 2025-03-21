1d7cf33cf9032ecaa4ebae247c7d2188a6231177
Luatop1 <timurr0830@gmail.com>
2025-03-22 00:16:40 +0300

    Changed files

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-time_input = str(input("Введите время:"))
-def time_to_minutes(user_time):
-    new_time = user_time.split(":")
+while True:
+    time_input = str(input("Введите время, например 12:07 :"))
+    new_time = time_input.split(":")
+    if int(new_time[0]) > 24 or int(new_time[0]) < 0 or int(new_time[1]) >= 60 or int(new_time[1]) < 0:
+        print("Введите правильно время!")
+    else:
+        break
+
+def time_to_minutes(user_time, new_time):
     minutes = new_time[0] = int(new_time[0]) * 60 + int(new_time[1])
     return minutes
 
-a = time_to_minutes(time_input)
-print(a)
+minutes = time_to_minutes(time_input, new_time)
+print(f"Ваше время в минутах: {minutes}")
